9a7ade659fcf7bf3f8a7912e06b196504826b97b
ntrouba <112958231+ntrouba@users.noreply.github.com>
2022-09-26 23:19:26 -0400

    Add files via upload

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,156 @@
-class Conversation {
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
-  public static void main(String[] arguments) {
+/**
+ * Class for user and bot conversation to occur
+ */
+class Conversation{
+  /**
+   * Execution of conversation begins here
+   */
+  public static void main(String[] arguments){
     // You will start the conversation here.
+
+    //Declaration of variables
+    var user_input = new Scanner(System.in);
+
+    //input = new Scanner(System.in);
+
+    System.out.println("How many rounds of conversation?");
+
+    //Variable that stores the number of rounds
+    int num_rounds = user_input.nextInt();
+
+    //List to save the transcript
+    ArrayList <String> transcript = new ArrayList<>();
+
+    //Random number generator
+    Random rand = new Random(); 
+
+    //List that stores conversational starters
+    ArrayList <String> convo_starter = new ArrayList<String>();
+    convo_starter.add("What are you thinking about?");
+    convo_starter.add("How was your day?");
+    convo_starter.add("What do you wanna talk about?");
+    convo_starter.add("What's up?");
+    convo_starter.add("Hi, how are you?");
+
+    //List that stores canned responses
+    ArrayList <String> canned_responses = new ArrayList<String>();
+    canned_responses.add("I see");
+    canned_responses.add("Very interesting");
+    canned_responses.add("Oh, really?");
+    canned_responses.add("Wow, that's crazy");
+    canned_responses.add("Me too");
+    canned_responses.add("I don't know much about that..");
+
+    //Prints line one
+    int index = rand.nextInt(convo_starter.size()-1);
+    String line_one = convo_starter.get(index); 
+    System.out.println(line_one);
+
+    //Adds line_one to the transcript
+    transcript.add(line_one);
+
+    //Defines scanner
+    Scanner response = new Scanner(System.in);
+
+    //for loop for the amount of rounds the user sellected
+    for (int i= 0; i< num_rounds; i++){
+      String user_response = response.nextLine();
+      user_response = user_response + " ";
+      transcript.add(user_response);
+
+      //First response
+      String bot_response = "";
+
+      String [] words = user_response.split(" ");
+      //Iterates through the words in the user response, and replaces the mirror words.
+      for (int r=0; r < words.length; r++){
+        if (words[r].equals("you")){
+          words[r] = "I";
+          bot_response = bot_response + words[r] +" ";
+        }
+          
+        else if (words[r].equals("I")||words[r].equals("i")){
+          words[r] = "you";
+          bot_response = bot_response + words[r] +" ";
+        }
+
+        else if (words[r].equals("are")){
+          words[r] = "am";
+          bot_response = bot_response + words[r] +" ";
+        }
+
+        else if (words[r].equals("was")){
+          words[r] = "were";
+          bot_response = bot_response + words[r] +" ";
+        }
+
+        else if (words[r].equals("am")){
+          words[r] = "are";
+          bot_response = bot_response + words[r] +" ";
+        }
+
+        else if (words[r].equals("my")){
+          words[r] = "your";
+          bot_response = bot_response + words[r] +" ";
+        }
+
+        else if (words[r].equals("your")){
+          words[r] = "my";
+          bot_response = bot_response + words[r] +" ";
+        }
+
+        else if (words[r].equals("me")){
+          words[r] = "you";
+          bot_response = bot_response + words[r] +" ";
+        }
+
+        else{
+          bot_response = bot_response + words[r] +" ";
+        }
+      }
+
+      //If the response has no mirror words it selects a canned response
+      if (user_response.equals(bot_response)){
+        int index2 = rand.nextInt(canned_responses.size()-1);
+        String canned_response = canned_responses.get(index2);
+        System.out.println(canned_response);
+        transcript.add(canned_response);
+      }
+
+      else{
+        System.out.println(bot_response);
+        transcript.add(bot_response);
+      }  
+
+  }
+
+  System.out.println("See ya next time!");
+  System.out.println("TRANSCRIPT");
+  System.out.println(transcript);
+
+  //Close scanners
+  user_input.close();
+  //Closes response scanner
+  response.close();
   }
 }
+
+
+
+  
+  
+   
+   
+
+
+
+
+    
+    
+   
+  
+
--- reflection.md
+++ reflection.md
@@ -5,3 +5,9 @@ Use this file to record your reflection on this assignment.
 - What are your initial impressions of Java? 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+ For this assignment I consulted with Lily Garner and Liberty Lehr. Lily and I attended the office hours on 9/25 from 7-9 and got help from Dakota. I struggled a bit with getting the conversation to execute for the right number of times but I figured out it was because of where my scanner closed. For future students, I recommend starting out with pseudocode because they know how to do this in Python but starting with pseudocode will make it easier to learn how to do what they want in Java.
+
+ I can't truly give you my "initial" impression of Java because funnily enough Java was my first programming language back in AP CSA my junior year of high school. It's been awhile since I've used it but it's fun to be programming with it again because I believe it offers more options than Python.
+ 
+ That transitions into the next question about the similarities between Python and Java. For me, it's about the same just different language to do the same things as before. Programming is just telling a computer to do things and just as there are different human languages there are several programming ones, each offering there own strengths and weaknesses.
